840dc30e10851ae0f0f0ae1ec430f678ae6156a3
tapan17102001 <56157717+tapan17102001@users.noreply.github.com>
2020-05-25 12:14:19 +0530

    Add files via upload

---
+++ base.cpp
@@ -0,0 +1,14 @@
+#define ll long long int;
+#define pb push_back;
+#define rep(i, a, b)
+for (int i = a; i < b; ++i)
+
+#define repd(i, a, b)
+    for (int i = a; i > b; --i)
+
+#define fastio
+        ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+#define vi vector<ll>;
+#define pl pair<ll, ll>;
+#define mod 1000000007;
