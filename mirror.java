1809da64882345dbc0739455b67d71fb1064d927
tapan17102001 <56157717+tapan17102001@users.noreply.github.com>
2020-05-26 16:35:04 +0530

    Add files via upload

---
+++ mirror.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[]) throws IOException
+   { 
+    
+    InputStreamReader read = new InputStreamReader(System.in);
+    BufferedReader in = new BufferedReader(read);
+    
+    String s = in.readLine();
+    System.out.println(s);
+   }
+ }
