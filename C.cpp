93e5f54e084869e0c0038a77c60e061a446a29ff
tapan17102001 <56157717+tapan17102001@users.noreply.github.com>
2020-07-19 13:14:28 +0530

    Add files via upload

--- C.cpp
+++ C.cpp
@@ -14,63 +14,9 @@ using namespace std;
 #define mod 1000000007
 #define PI 3.141592653589793238
 
-void solve()
-{
-    int n, k, d, mini = 100000;
-    cin >> n >> k >> d;
-
-    vector<pair<int, int>> a;
-    set<int> s;
-
-    for (int i = 0; i < n; i++)
-    {
-        int x;
-        cin >> x;
-
-        s.insert(x);
-        a.pb(mp(x, s.size()));
-    }
-
-    int x = d - 1, p = 0, L = 0, R = 0;
-    for (int i = 0; i <= n - d; i++)
-    {
-
-        int l = a[i].second;
-        int r = a[i + x].second;
-
-        if (r == l)
-            p = a[i].second;
-        else
-            p = (r - l) + 1;
-
-        if (p <= mini)
-        {
-            mini = p;
-            L = i;
-            R = i + x;
-        }
-    }
-
-    set<int> q;
-    for (int i = L; i <= R; i++)
-    {
-        q.insert(a[i].first);
-    }
-
-    cout << q.size() << endl;
-}
-
 int main()
 {
     IOS;
-
-    int t;
-    cin >> t;
-
-    while (t--)
-    {
-        solve();
-    }
 
     return 0;
 }
